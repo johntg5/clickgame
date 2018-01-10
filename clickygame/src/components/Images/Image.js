5aba1fd54ba351ce169478bf49ee820b6389a91a
Tyler Johnson <johntg5@gmail.com>
2018-01-10 21:58:07 +0000

    fixing image.js

--- clickygame/src/components/Images/Image.js
+++ clickygame/src/components/Images/Image.js
@@ -3,7 +3,7 @@ import './Image.css';
 
 const Image = props => (
     <div className='img-thumbnail imgWrapper'>
-        <img onClick={() => props.sortImages(props.id, props.clicked)} id={props.id} src={props.src} className='img-fluid imgSize gridEle ' alt="rick and morty character" data-clicked={props.clicked}/>
+        <img onClick={() => props.sortImages(props.id, props.clicked)} id={props.id} src={props.src} className='img-fluid imgSize gridEle ' alt="bojack character" data-clicked={props.clicked}/>
     </div>
 );
 
